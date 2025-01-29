21aa7fc44afd665665dd421107fab5929c22da66
Christian <haxutoasty@gmail.com>
2025-01-29 01:46:33 -0600

    corrected unittext string

--- script.js
+++ script.js
@@ -2,18 +2,25 @@ function convert(){
 
     const inputValue = document.getElementById('userInput').value;
     const unit = document.getElementById('unit').value;
-    
     let result = 0;
+    let unitText = '';
     
     if (unit === "milesToKm") {
      result = inputValue * 1.60934;
+     unitText = ' miles are ';
     } else {
      result = inputValue / 1.60934;
+     unitText = ' kilometers are ';
     }
     
-    const resultString = inputValue + " miles are " + result + " km";
+    const resultString = inputValue + unitText + result + (unit === "milesToKm" ? ' km' : ' miles');
     console.log(resultString);
+    // if (unit === "milesToKm") {
+    //     return " km";
+    // } else {
+    //     return " miles";
+    // }
     
     const resultElement = document.getElementById('resultElement');
     resultElement.innerHTML = resultString;
-}
+  }
